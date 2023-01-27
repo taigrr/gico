0e40bbab31c7d6ffd122db50f3106ff496ac41c3
Tai Groot <tai@taigrr.com>
2023-01-26 23:51:11 -0800

    add types package

---
+++ types/helpers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+)
+
+func NewDataSet() DataSet {
+	return make(DataSet)
+}
+
+func NewCommit(Author, Message, Repo, Path string, LOC int) Commit {
+	ci := Commit{
+		Message: Message,
+		Author:  Author, LOC: LOC, TimeStamp: time.Now(),
+		Repo: Repo, Path: Path,
+	}
+	return ci
+}
---
+++ types/types.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+)
+
+type Month string
+
+type Commit struct {
+	LOC       int       `json:"loc,omitempty"`
+	Message   string    `json:"message,omitempty"`
+	TimeStamp time.Time `json:"ts,omitempty"`
+	Author    string    `json:"author,omitempty"`
+	Repo      string    `json:"repo,omitempty"`
+	Path      string    `json:"path,omitempty"`
+}
+
+type DataSet map[time.Time]WorkDay
+
+type WorkDay struct {
+	Day     time.Time `json:"day"`
+	Count   int       `json:"count"`
+	Commits []Commit  `json:"commits,omitempty"`
+}
