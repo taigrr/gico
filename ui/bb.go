7aa193c870090b05c113ce6bc0fbc5a510020e37
Tai Groot <tai@taigrr.com>
2023-01-26 23:57:38 -0800

    add df code for UI

---
+++ ui/bb.go
@@ -0,0 +1,23 @@
+package ui
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/taigrr/gico/graph"
+)
+
+func InteractiveGraph() {
+	m := graph.New()
+	df := lipgloss.NewDoeFoot()
+	m = m.UpdateDoeFoot(df)
+
+	p := tea.NewProgram(m)
+	if err := p.Start(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+
+}
