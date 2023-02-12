d67287bbb8ae3476d9eb90f4ae78564cea9b760d
Tai Groot <tai@taigrr.com>
2023-02-11 22:01:17 -0800

    remove xmain

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -8,31 +8,7 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/taigrr/gico/commits"
-	"github.com/taigrr/gico/graph/term"
 )
-
-func xmain() {
-	n := time.Now()
-	repoPaths, err := commits.GetMRRepos()
-	if err != nil {
-		panic(err)
-	}
-	freq, err := repoPaths.Frequency(n.Year(), []string{"Groot"})
-	if err != nil {
-		panic(err)
-	}
-	wfreq, err := repoPaths.GetWeekFreq([]string{"Groot"})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("week:")
-	fmt.Println(term.GetWeekUnicode(wfreq))
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println("year:")
-	fmt.Println(term.GetYearUnicode(freq))
-}
 
 type errMsg error
 
